5a563795804f467b1bd6d8b965964a5e9d581b4d
rbutor <92458249+rbutor@users.noreply.github.com>
2022-02-20 20:54:32 +0300

    Update Print-RandomImage.ps1

--- Print-RandomImage.ps1
+++ Print-RandomImage.ps1
@@ -1,3 +1,3 @@
 $myDownloadUrl = 'https://bing.ioliu.cn/v1/rand'
 Invoke-WebRequest $myDownloadUrl -OutFile C:\jpg\1.jpg
-mspaint.exe /p "c:\jpg\1.jpg"
+Start-Process -FilePath 'C:\Windows\System32\mspaint.exe' -ArgumentList '/p "C:\jpg\1.jpg"'
