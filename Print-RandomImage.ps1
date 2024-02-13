7b911a4b5e653bbfdfd1bd27fa659466f3e2830b
rbutor <92458249+rbutor@users.noreply.github.com>
2024-02-13 21:24:05 +0300

    Changre Pic Source

--- Print-RandomImage.ps1
+++ Print-RandomImage.ps1
@@ -1,3 +1,5 @@
-$myDownloadUrl = 'https://bing.ioliu.cn/v1/rand?w=1920&h=1200'
+#$myDownloadUrl = 'https://bing.ioliu.cn/v1/rand?w=1920&h=1200'
+#$myDownloadUrl = 'https://unsplash.it/1600/1130?random'
+$myDownloadUrl = 'https://loremflickr.com/2000/1300/abstract'
 Invoke-WebRequest $myDownloadUrl -OutFile C:\jpg\1.jpg
-Start-Process -FilePath 'C:\Windows\System32\mspaint.exe' -ArgumentList '/p "C:\jpg\1.jpg"'
+Start-Process -FilePath 'mspaint.exe' -ArgumentList '/p "C:\jpg\1.jpg"'
